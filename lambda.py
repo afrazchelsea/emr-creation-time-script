93f6e9c4f34fd4a1aa12695536ec508882d6cbab
afrazchelsea <afraz.momin@gmail.com>
2022-11-09 03:44:25 +0530

    code clean-up

--- lambda.py
+++ lambda.py
@@ -17,19 +17,15 @@ def lambda_handler(event, context):
     long_running_clusters = []
     emr_client = boto3.client('emr')
 
-    # clusters = emr_client.list_clusters(ClusterStates=['RUNNING'])
-    clusters = event
-    print("Clusters---->", clusters)
+    clusters = emr_client.list_clusters(ClusterStates=['RUNNING'])
+    #clusters = event
 
     for cluster in clusters['Clusters']:
         cluster_data=[]
         cluster_id = cluster['Id']
-        print("Cluster ID - ", cluster_id)
         cluster_launch_time = str(cluster['Status']['Timeline']['CreationDateTime']).split("+")[0].split(".")[0]
         creation_date = cluster_launch_time.split(" ")[0]
-        print("Cluster Launch Time - ", cluster_launch_time)
         elapsed_time = calc_elapsed_time(cluster_launch_time)
-        print("Elapsed Time - ", elapsed_time)
         cluster_data.append(cluster_id)
         cluster_data.append(creation_date)
         cluster_data.append(int(elapsed_time))
@@ -37,15 +33,12 @@ def lambda_handler(event, context):
             long_running_clusters.append(cluster_data)
         else:
             new_clusters.append(cluster_data)    
-            
-    print("Long Running - ", long_running_clusters)
-    print("New Ones - ", new_clusters)
     
-    print("***Long Running Clusters***")
-    tabulate_data(long_running_clusters)
-    print("***Newly Created Clusters***")
+    print("\n---Newly Created Clusters---")
     tabulate_data(new_clusters)
-    
+    print("---Long Running Clusters---")
+    tabulate_data(long_running_clusters)
+
     return {
         'statusCode': 200,
         'body': json.dumps('Execution Completed Successfully!')
@@ -58,15 +51,12 @@ def utc_now():
 
 
 def calc_elapsed_time(launch_time):
-    print("---In calc_elapsed_time---")
     launch_dt = datetime.datetime.strptime(launch_time, "%Y-%m-%d %H:%M:%S")
-    print(launch_dt)
     elapsed_time = utc_now() - launch_dt
-    print(elapsed_time)
     diff_minutes = (elapsed_time.days * 24 * 60) + (elapsed_time.seconds/60)
-    print(diff_minutes)
     return diff_minutes/60
     
 def tabulate_data(data_list):
     head = ["EMR_ID", "Creation_Data", "Elapsed_Time"]
     print(tabulate(data_list, headers=head, tablefmt="grid"))
+    
